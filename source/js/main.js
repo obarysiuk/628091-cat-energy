c540ac12fcb69483654bd47301a04e20183a162a
Olga Barysiuk <o.barysiuk@gmail.com>
2018-02-14 13:56:28 -0500

    исправлен тип кавычек в main.js

--- source/js/main.js
+++ source/js/main.js
@@ -1,23 +1,23 @@
 // главное меню
 
-var navMain = document.querySelector('.main-nav');
-    var navToggle = document.querySelector('.page-header__btn');
-var headerNojs = document.querySelector('.page-header--nojs');
+var navMain = document.querySelector(".main-nav");
+    var navToggle = document.querySelector(".page-header__btn");
+var headerNojs = document.querySelector(".page-header--nojs");
 
 if(headerNojs) {
-  headerNojs.classList.remove('page-header--nojs');
+  headerNojs.classList.remove("page-header--nojs");
 }
 
-    navToggle.addEventListener('click', function () {
-      if (navMain.classList.contains('main-nav--closed')) {
-        navMain.classList.remove('main-nav--closed');
-        navMain.classList.add('main-nav--opened');
-        navToggle.classList.add('menu-opened');
+    navToggle.addEventListener("click", function () {
+      if (navMain.classList.contains("main-nav--closed")) {
+        navMain.classList.remove("main-nav--closed");
+        navMain.classList.add("main-nav--opened");
+        navToggle.classList.add("menu-opened");
 
       } else {
-        navMain.classList.add('main-nav--closed');
-        navMain.classList.remove('main-nav--opened');
-        navToggle.classList.remove('menu-opened');
+        navMain.classList.add("main-nav--closed");
+        navMain.classList.remove("main-nav--opened");
+        navToggle.classList.remove("menu-opened");
       }
     });
 
@@ -27,10 +27,10 @@ function validateForm(form) {
   for (var i=0; i<form.elements.length; i++) {
     var element = form.elements[i];
 
-    element.classList.remove('form-page__input-text--error');
+    element.classList.remove("form-page__input-text--error");
 
     if (element.required && !element.value) {
-      element.classList.add('form-page__input-text--error');
+      element.classList.add("form-page__input-text--error");
     }
   }
 }
@@ -39,7 +39,7 @@ function validateForm(form) {
 
 function initMap() {
   var uluru = {lat: 37.721631, lng: -122.459751};
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 15,
     center: uluru
   });
