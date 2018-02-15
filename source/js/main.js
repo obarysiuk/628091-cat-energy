ee80f595eefe17f6e2a5188d825a47b54304f19c
Olga Barysiuk <o.barysiuk@gmail.com>
2018-02-15 09:58:19 -0500

    иконки меню в спрайт, замена иконок .pagination

--- source/js/main.js
+++ source/js/main.js
@@ -1,30 +1,29 @@
 // главное меню
 
 var navMain = document.querySelector(".main-nav");
-    var navToggle = document.querySelector(".page-header__btn");
+var navToggle = document.querySelector(".page-header__btn");
 var headerNojs = document.querySelector(".page-header--nojs");
 
-if(headerNojs) {
+if (headerNojs) {
   headerNojs.classList.remove("page-header--nojs");
 }
 
-    navToggle.addEventListener("click", function () {
-      if (navMain.classList.contains("main-nav--closed")) {
-        navMain.classList.remove("main-nav--closed");
-        navMain.classList.add("main-nav--opened");
-        navToggle.classList.add("menu-opened");
-
-      } else {
-        navMain.classList.add("main-nav--closed");
-        navMain.classList.remove("main-nav--opened");
-        navToggle.classList.remove("menu-opened");
-      }
-    });
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+    navToggle.classList.add("menu-opened");
+  } else {
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
+    navToggle.classList.remove("menu-opened");
+  }
+});
 
 // валидация формы
 
 function validateForm(form) {
-  for (var i=0; i<form.elements.length; i++) {
+  for (var i = 0; i < form.elements.length; i++) {
     var element = form.elements[i];
 
     element.classList.remove("form-page__input-text--error");
@@ -38,7 +37,7 @@ function validateForm(form) {
 // карта
 
 function initMap() {
-  var uluru = {lat: 37.721631, lng: -122.459751};
+  var uluru = { lat: 37.721631, lng: -122.459751 };
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 15,
     center: uluru
@@ -46,6 +45,5 @@ function initMap() {
   var marker = new google.maps.Marker({
     position: uluru,
     map: map
-
   });
 }
